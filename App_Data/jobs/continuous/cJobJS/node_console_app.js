e4458670fb435087c83a9f581cd06b48c42cbda8
amitapl <amitapl@outlook.com>
2014-04-21 15:18:27 -0700

    fix

--- App_Data/jobs/continuous/cJobJS/node_console_app.js
+++ App_Data/jobs/continuous/cJobJS/node_console_app.js
@@ -1,11 +1,21 @@
 var fs = require('fs');
 
+var shutdownDetectedFilePath = 'd:\\home\\site\\shutdown.detected';
+
+try {
+  fs.unlinkSync(shutdownDetectedFilePath);
+} catch (e) {
+}
+
 function whileLoop() {
   console.log('In while loop');
   if (!fs.existsSync(process.env['WEBJOBS_SHUTDOWN_FILE'])) {
     setTimeout(whileLoop, 1000);
   } else {
     console.log('Shutdown detected');
+
+    // Write file to show shutdown was detected
+    fs.writeFileSync('d:\\home\\site\\shutdown.detected', 'Shutdown detected');
   }
 }
 
--- App_Data/jobs/triggered/tJobJs/node_console_app_triggered.js
+++ App_Data/jobs/triggered/tJobJs/node_console_app_triggered.js
@@ -4,7 +4,7 @@ var counter = 0;
 // Run for 10 seconds and finish
 function whileLoop() {
   console.log('In while loop ' + (counter++));
-  if (counter > 10) {
+  if (counter <= 10) {
     setTimeout(whileLoop, 1000);
   } else {
     console.log('Work done');
